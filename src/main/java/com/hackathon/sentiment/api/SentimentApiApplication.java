4919819a5cbada0726138dd9e7c2f0636f045556
yober cieza <gctic.sgsass12@essalud.gob.pe>
2025-12-17 22:57:59 -0500

    Proyecto inicial

---
+++ src/main/java/com/hackathon/sentiment/api/SentimentApiApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.sentiment.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/sentiment/api/SentimentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.sentiment.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
