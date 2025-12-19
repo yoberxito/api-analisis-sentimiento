22e9f813aa16bad0aa2a11795479923be8718cb4
WAV9874 <acosta_vela_wilmer@hotmail.com>
2025-12-19 01:15:25 -0500

    Initial commit

---
+++ src/main/java/com/hackathon/sentiment/api/SentimentApiApplication.java
@@ -0,0 +1,15 @@
+package com.hackathon.sentiment.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentApiApplication {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		SpringApplication.run(SentimentApiApplication.class, args);
+	}
+
+}
