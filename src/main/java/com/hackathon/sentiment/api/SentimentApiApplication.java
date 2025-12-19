8e1c4912dd94cb1ca9c3cac62ec9e09f5748486d
yober cieza <gctic.sgsass12@essalud.gob.pe>
2025-12-18 20:31:30 -0500

    cambios

--- src/main/java/com/hackathon/sentiment/api/SentimentApiApplication.java
+++ src/main/java/com/hackathon/sentiment/api/SentimentApiApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SentimentApiApplication {
 
+
 	public static void main(String[] args) {
+		System.out.println("Hola");
 		SpringApplication.run(SentimentApiApplication.class, args);
 	}
 
