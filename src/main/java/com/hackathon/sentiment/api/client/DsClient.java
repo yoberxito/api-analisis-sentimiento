38b7562cf2adc59fdb18bbc58240c50edd983c41
WAV9874 <acosta_vela_wilmer@hotmail.com>
2026-01-07 20:24:26 -0500

    Ajusta DsClient y configuración en application.properties

--- src/main/java/com/hackathon/sentiment/api/client/DsClient.java
+++ src/main/java/com/hackathon/sentiment/api/client/DsClient.java
@@ -1,15 +1,6 @@
-package com.hackathon.sentiment.api.client;/*
- * Copyright (c) 2025 yober cieza coronel. Todos los derechos reservados.
- *
- * Este archivo es parte de api-analisis-sentimiento.
- *
- * api-analisis-sentimiento es software propietario: no puedes redistribuirlo y/o modificarlo sin el
- * permiso expreso del propietario. Está sujeto a los términos y condiciones
- * que acompañan el uso del software.
- *
- * Cualquier uso no autorizado puede ser sancionado según la ley vigente.
- */
+package com.hackathon.sentiment.api.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,18 +8,19 @@ import java.util.Map;
 
 @Service
 public class DsClient {
+
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String DS_URL = "http://localhost:8000/predict";
+
+    @Value("${ds.url}")
+    private String dsUrl;
 
     public Map<String, Object> predict(String text) {
-
         Map<String, String> body = Map.of("text", text);
 
         return restTemplate.postForObject(
-                DS_URL,
+                dsUrl,
                 body,
                 Map.class
         );
     }
-
 }
